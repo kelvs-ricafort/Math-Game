8ea1d80805834360315482dfe1ed5f5283e24960
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-19 14:15:59 +0800

    Setup Math game Main Page

--- app/src/main/java/com/newtechieblog/wordpress/views/mathgame/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/mathgame/MainActivity.java
@@ -2,13 +2,32 @@ package com.newtechieblog.wordpress.views.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addition;
+    Button subtraction;
+    Button multiplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addition = findViewById(R.id.btnAdd);
+        subtraction = findViewById(R.id.btnSubtract);
+        multiplication = findViewById(R.id.btnMultiply);
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Game.class);
+                startActivity(intent);
+            }
+        });
     }
 }
